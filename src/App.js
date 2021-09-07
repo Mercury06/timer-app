80e8e8ad99d6f9d9ed03314d0280c046ae52f4bc
Mercury06 <zetmail@ukr.net>
2021-09-07 17:25:30 +0300

    with reset button

--- src/App.js
+++ src/App.js
@@ -1,55 +1,65 @@
+import React from 'react';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { interval, startWith, Subject, switchMap, timeout } from 'rxjs';
+
+
+const observable$ = interval(1000);
+const actionReset$ = new Subject ();
+actionReset$.subscribe(console.log)
+
 
 function App() {
+
   const [ time, setTime] = useState (0);
-  const [ timerOn, setTimerOn ] = useState (false);
+  const [ timerOn, setTimerOn ] = useState (false); 
+
+  useEffect(() => {                   // для отладки
+    console.log('useEffect:', time);  
+  }, [time])
 
   useEffect(() => {
-    let interval = null;
-
-    if (timerOn) {
-      interval = setInterval(() => {
-        setTime(prevTime => prevTime + 10)
-      }, 10)
-    } else {
-      clearInterval(interval)
-    }
-    return () => clearInterval(interval)
-  }, [timerOn])
+          
+    let subscription = actionReset$.pipe(
+      startWith(null),
+      switchMap(() => observable$),
+    ).subscribe(result =>{
+      timerOn && setTime(result);
+      } 
+    );
+    return () => subscription.unsubscribe();
+  }, [timerOn]);
 
   const onStopClick = () => {
     setTimerOn(false)
     setTime(0)
-    console.log(time)
   }
 
   return (
     <div className="mainBlock">
-      <div >
-      <div className="numbersBlock">
-        <span>{("0" + Math.floor(( time / 60000) % 60 )).slice(-2)} :</span>&nbsp;
-        <span>{("0" + Math.floor(( time / 1000) % 60 )).slice(-2)} :</span>&nbsp;
-        <span>{("0" + (( time / 10) % 100)).slice(-2)}</span>
-      </div>     
-      <div className="buttonsBlock">
-        {!timerOn && (
-        <button onClick={() => setTimerOn(true)}>Start</button>
-        )}
-        { timerOn && (
-        <button onClick={onStopClick}>Stop</button>
-        )}
-        { timerOn && (
-        <button onClick={() => setTimerOn(false)}>Wait</button>
-        )}   
-        { time > 0 && (
-        <button onClick={() => setTime(0)}>Reset</button>
-        )}
-      </div>
-      </div>    
-    </div>
+        <div>
+            <div className="numbersBlock">
+              <span>{("0" + Math.floor(( time / 3600) % 60 )).slice(-2)} :</span>&nbsp;
+              <span>{("0" + Math.floor(( time / 60) % 60 )).slice(-2)} :</span>&nbsp;
+              <span>{("0" + Math.floor(( time) % 60 )).slice(-2)}</span>    
+            </div>     
+            <div className="buttonsBlock">
+              {!timerOn && (
+              <button onClick={() => setTimerOn(true)}>Start</button>
+              )}
+              { timerOn && (
+              <button onClick={onStopClick}>Stop</button>
+              )}
+              { timerOn && (
+              <button onClick={() => actionReset$.next(true)}>Wait</button>
+              )}   
+              { time > 0 && (
+              <button onClick={() => actionReset$.next('clickedreset')}>Reset</button>
+              )}   
+            </div>    
+        </div>
+    </div>        
   );
 }
 
 export default App;
-
