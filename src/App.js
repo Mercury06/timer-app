449a0456e9edeafe681f5344d0849ac70a43ab80
Mercury06 <zetmail@ukr.net>
2021-09-13 13:26:21 +0300

    edited timer with buttons

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
 import React from 'react';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { interval, startWith, Subject, switchMap, scan, filter, share, tap, merge, EMPTY, mapTo, takeUntil, bufferCount, map} from 'rxjs';
+import { interval, Subject, filter, map, debounceTime, buffer} from 'rxjs';
 
 
-const observable$ = interval(1000).pipe(mapTo(1), share());
+const observable$ = interval(1000);
 
 const action$ = new Subject ();
 action$.subscribe(console.log)
+const wait$ = action$.pipe(filter( action => action === "wait"));
 
-
-//const wait$ = action$.pipe(filter( action => action === false));
-
-const wait$ = action$.pipe(filter( action => action === "wait"), map(action=>action === false));
-
-//const wait$ = action$.pipe(filter( action => action === "wait"), bufferWhen(() => action$.debounceTime(500)), map(list => list.length), filter(length => length >= 2), map(action=>action === false));
-
-const start$ = action$.pipe(filter( action => action === true));
-
-const stop$ = action$.pipe(filter( action => action === "clickstop"));
-
-const reset$ = action$.pipe(filter( action => action === "clickreset"));
-
-const timer$ = merge(wait$, start$)
-  .pipe(
-    startWith(false),    
-    switchMap(val => (val ? observable$.pipe(takeUntil(reset$.pipe(tap(()=>console.log('reseted')), switchMap(()=>observable$.pipe(startWith(null)))))) : EMPTY)),
-    scan((acc, curr) => (curr ? curr + acc : acc))
-  )
 
 function App() {
 
   const [ time, setTime] = useState (0);
-  const [ timerOn, setTimerOn ] = useState (true); 
+  const [ timerOn, setTimerOn ] = useState (false); 
 
   useEffect(() => {                   // для отладки
     console.log('useEffect:', time);  
-  }, [time])
-
-  
-  useEffect(() => {     
-
-    let subscription = timer$.subscribe 
-    (result =>{setTime(result);
-      } 
-    );
+  }, [time])  
+ 
+  useEffect(() => {
+    let subscription = observable$
+    .subscribe(() => {
+          if (timerOn) {
+            setTime((prev) => prev + 1)
+          };
+        });
     return () => subscription.unsubscribe();
-  }, []);  
+  }, [timerOn]); 
+
+
+  useEffect (() => {
+    const counts = wait$.pipe(debounceTime(300));
+    const clicked$ = wait$.pipe(buffer(counts), map( clicks => clicks.length), filter( clicksCount => clicksCount === 2));  
+    const waitMode = clicked$.subscribe(() => {
+      setTimerOn(false);
+    });    
+    return () =>  waitMode.unsubscribe();
+  }, []);
+
+  const onStopClicked = () => {
+    setTimerOn(false)
+    setTime(0)
+  }
 
   return (
     <div className="mainBlock">
@@ -57,18 +54,18 @@ function App() {
               <span>{("0" + Math.floor(( time) % 60 )).slice(-2)}</span>    
             </div>     
             <div className="buttonsBlock">
-              { (
-              <button className="start" onClick={() => action$.next(true)}>Start</button>
+              { !timerOn && (
+              <button className="start" onClick={() => setTimerOn(true)}>Start</button>
               )}
               { timerOn && (
-              <button className="stop" onClick={() => action$.next("clickstop")}>Stop</button>
+              <button className="stop" onClick={onStopClicked}>Stop</button>
               )}
               { timerOn && (
               <button className="wait" onClick={() => action$.next("wait")}>Wait</button>
               )}   
               { time > 0 && (
-              <button className="reset" onClick={() => action$.next("clickreset")}>Reset</button>
-              )}   
+              <button className="reset" onClick={()=>setTime(0)}>Reset</button>
+              )}               
             </div>    
         </div>
     </div>        
